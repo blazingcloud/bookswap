e241d4553e1f80bcd06df18338ee6aab9d5e9776
Sarah Allen <sarah@ultrasaurus.com>
2011-05-30 18:27:13 -0700

    added person model with spec, homework from class #3

---
+++ app/models/person.rb
@@ -0,0 +1,7 @@
+class Person < ActiveRecord::Base
+  validates_presence_of :given_name, :surname
+  
+  def full_name
+    "#{given_name} #{surname}"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110510004539) do
+ActiveRecord::Schema.define(:version => 20110531011729) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
     t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "people", :force => true do |t|
+    t.string   "given_name"
+    t.string   "surname"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
