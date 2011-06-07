f463c4f1df1fc93e1b9f642dc3498f2cc5b2ea6b
Curtis Schofield <github.com@robotarmyma.de>
2011-06-06 19:01:37 -0700

    seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bookswap::Application.routes.draw do
   resources :books
   resources :people
+<<<<<<< HEAD
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -57,4 +58,6 @@ Bookswap::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+=======
+>>>>>>> seeds
 end
