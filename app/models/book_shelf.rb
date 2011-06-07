0d6702dc5ee5a931b837e6221f549d9494d8ea18
Curtis Schofield <github.com@robotarmyma.de>
2011-06-06 20:25:41 -0700

    prep homework branch

--- app/models/book_shelf.rb
+++
@@ -1,4 +0,0 @@
-class BookShelf < ActiveRecord::Base
-  belongs_to :person
-  belongs_to :book
-end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,4 @@
 class Person < ActiveRecord::Base
-  has_many  :book_shelfs
-  has_many  :books, :through => :book_shelfs
   validates_presence_of :given_name, :surname
 
   def full_name
