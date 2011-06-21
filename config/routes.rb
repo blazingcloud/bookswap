7074156a98d831f488b0b2becf1125ee7d7676ec
Curtis Schofield <github.com@robotarmyma.de>
2011-06-20 19:40:00 -0700

    devise install steps 2/3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bookswap::Application.routes.draw do
   resources :books
   resources :people
+  root :to => "books#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
